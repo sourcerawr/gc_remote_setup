96e7fd9e04857efc38302c7edd0cdc995a2b668e
Ashwary Sharma <ashwary0102@gmail.com>
2024-05-21 12:23:13 +0000

    Commenting p10k

--- remote_setup.sh
+++ remote_setup.sh
@@ -72,9 +72,9 @@ then
     sudo apt-get install zsh
     chsh -s $(which zsh)
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-    echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>~/.zshrc
-    source ~/.zshrc
+    # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+    # echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>~/.zshrc
+    # source ~/.zshrc
 else
     echo "Skipping shell upgrade..."
 fi
