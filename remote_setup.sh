17baa860debbc3bc546dcd558178d0124748614a
ashwary0102 <ashwary0102@gmail.com>
2024-08-09 22:47:19 +0530

    Added some VS Code extensions - will add rest post testing

--- remote_setup.sh
+++ remote_setup.sh
@@ -79,3 +79,15 @@ then
 else
     echo "Skipping shell upgrade..."
 fi
+
+# installing VS Code extensions - python, copilot, jupyter, errorlens, data wranger, gitlens - let's first see which all are needed
+# asking
+read -p "Install VS Code extensions? (y/n) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    code --install-extension ms-python.python
+    code --install-extension github.copilot
+    code --install-extension ms-toolsai.jupyter
+else
+    echo "Skipping VS Code extensions..."
+fi
